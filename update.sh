cba2b1795e0e1e8b4ef5d87201b8e6e68d77b832
Manuel Batsching <m.batsching@abtis.de>
2022-01-13 16:09:37 +0100

    use single submodule for posts and static

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
 git submodule update --recursive --remote --init
-mv ./content/post/static/* static
-cd ./content/post
-git filter-branch --subdirectory-filter posts -f
-cd ../../
+cp -r content/static .
